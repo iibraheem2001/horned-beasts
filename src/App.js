690f4aedfcfbef651737f5e9ab777c4bb8a2c3c0
ibraheem ibraheem <iibraheem2001@gmail.com>
2021-10-25 21:06:10 -0700

    worked on initial hornedbeasts lab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+
+export default class App extends Component {
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +24,14 @@ function App() {
         </a>
       </header>
     </div>
+    <main>
+  function HornedBeast(params) {
+    return ()
+  }
+    </main>
+    <footer>
+      <p>Author: Ibraheem Ibraheem</p>
+    </footer>
   );
 }
 
